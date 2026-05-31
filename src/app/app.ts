91f9237c2d3117b179e526f421fefa966ee5c650
Dorian <contact@dorian-naaji.fr>
2026-05-31 16:52:44 +0200

    refactor: remove dev-reset feature and simplify env configuration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DevResetComponent } from './shared/components/dev-reset/dev-reset.component';
-import { environment } from '../environments/environment';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DevResetComponent, CommonModule],
+  imports: [RouterOutlet, CommonModule],
   template: `
     <router-outlet />
-    @if (!isProd) {
-      <app-dev-reset />
-    }
   `,
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('nutrisnap');
-  isProd = environment.production;
-
-  constructor() {
-    console.log('Environment:', environment);
-  }
 }
